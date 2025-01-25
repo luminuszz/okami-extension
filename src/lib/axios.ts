c4efd68061d1ac3a2d02995be0a65d2f64e00b22
luminuszz <luminuszz43@gmail.com>
2025-01-25 13:11:23 -0300

    fix: auth flux

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,8 +1,8 @@
 import axios, { AxiosError } from 'axios'
 
-import { getTokensByExtensionStorage } from '@/lib/storage.ts'
 import { refreshTokenCall } from '@/api/refresh-token.ts'
 import { eventBridge } from '@/lib/events.ts'
+import { getTokensByExtensionStorage } from '@/lib/storage.ts'
 
 export const okamiHttpGateway = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
@@ -10,24 +10,21 @@ export const okamiHttpGateway = axios.create({
 
 let isRefreshing = false
 
-export type FailRequestQueue = Array<{
+type FailRequestQueue = {
   onSuccess: (newToken: string) => void
   onFailure: (error: AxiosError) => void
-  id: string
-}>
+}[]
 
-const failRequestQueue: FailRequestQueue = []
-
+let failRequestQueue: FailRequestQueue = []
 
 export const isUnauthorizedError = (error: AxiosError): boolean =>
   [401, 403].includes(error.response?.status || 0)
 
-
 okamiHttpGateway.interceptors.request.use(
-  async  requestConfig => {
+  async (requestConfig) => {
     const { token } = await getTokensByExtensionStorage()
 
-    eventBridge.emit("axios.makeRequest")
+    eventBridge.emit('axios.makeRequest')
 
     if (token) {
       requestConfig.headers.Authorization = `Bearer ${token}`
@@ -35,61 +32,52 @@ okamiHttpGateway.interceptors.request.use(
 
     return requestConfig
   },
-  error => {
+  (error) => {
     return Promise.reject(error)
-  }
+  },
 )
-
 
 okamiHttpGateway.interceptors.response.use(
   (response) => response,
-  async (exception:AxiosError) => {
-    const { refreshToken } = await getTokensByExtensionStorage()
+  async (exception: AxiosError) => {
+    if (isUnauthorizedError(exception)) {
+      getTokensByExtensionStorage().then(({ refreshToken }) => {
+        const canStartRefreshTokenFlow = refreshToken && !isRefreshing
 
-    const canStartRefreshTokenFlow =
-      isUnauthorizedError(exception) && refreshToken && !isRefreshing
+        if (canStartRefreshTokenFlow) {
+          isRefreshing = true
+        }
 
-    if (!canStartRefreshTokenFlow) {
-      return Promise.reject(exception)
+        refreshTokenCall(refreshToken)
+          .then(({ token }) => {
+            failRequestQueue.forEach((request) => {
+              chrome.storage.local.set({ token })
+              request.onSuccess(token)
+            })
+          })
+          .catch((error) => {
+            failRequestQueue.forEach((request) => {
+              eventBridge.emit('user.isUnauthenticated')
+              chrome.storage.local.remove(['token', 'refreshToken'])
+              request.onFailure(error)
+            })
+          })
+          .finally(() => {
+            isRefreshing = false
+            failRequestQueue = []
+          })
+      })
     }
-
-    const callInAlreadyInQueue = failRequestQueue.find(
-      (item) => item.id === exception.config?.url,
-    )
-
-    if (callInAlreadyInQueue) {
-      return Promise.reject(exception)
-    }
-
-    isRefreshing = true
-
-    refreshTokenCall(refreshToken)
-      .then(({ token }) => {
-        failRequestQueue.forEach((request) => {
-          chrome.storage.local.set({ token })
-          request.onSuccess(token)
-        })
-      })
-      .catch((error) => {
-        failRequestQueue.forEach((request) => {
-          eventBridge.emit("user.isUnauthenticated")
-          request.onFailure(error)
-        })
-      })
-      .finally(() => {
-        isRefreshing = false
-      })
 
     return new Promise((resolve, reject) => {
       failRequestQueue.push({
-        id: exception.config?.url || '',
         onFailure: (error) => reject(error),
         onSuccess: async (token: string) => {
           if (!exception.config?.headers) return
 
           exception.config.headers.Authorization = `Bearer ${token}`
 
-          resolve(okamiHttpGateway(exception.config))
+          return resolve(okamiHttpGateway(exception.config))
         },
       })
     })
