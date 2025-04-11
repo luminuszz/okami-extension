55a9921a378b7b923bf381bb78357f16f59da3ab
luminuszz <luminuszz43@gmail.com>
2025-04-10 21:28:00 -0300

    feat: add sso to extension

---
+++ src/pages/login-with-platform.tsx
@@ -0,0 +1,23 @@
+import { Container } from '@/components/container'
+import { Logo } from '@/components/logo.tsx'
+import { Button } from '@/components/ui/button'
+
+export function LoginWithPlatform() {
+  async function handleRedirectToOkami() {
+    await chrome.tabs.create({
+      url: `${import.meta.env.VITE_OKAMI_PTATFORM_URL}/auth/auth-extension`,
+      active: true,
+    })
+  }
+
+  return (
+    <Container>
+      <section className="flex flex-col gap-4">
+        <picture className="flex justify-center">
+          <Logo className="size-28" />
+        </picture>
+        <Button onClick={handleRedirectToOkami}>Entrar com Okami</Button>
+      </section>
+    </Container>
+  )
+}
