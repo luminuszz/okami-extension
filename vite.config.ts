952b4ca4543d354e72e0a5dde14774afea8b51b4
luminuszz <luminuszz43@gmail.com>
2025-07-23 23:03:32 -0300

    make build for firefix

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,20 @@ import path from 'node:path'
 
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: './manifest.json',
+          dest: '.',
+        },
+      ],
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
